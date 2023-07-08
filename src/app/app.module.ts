8238f85b70968d6644bc1edbcc8a117eadea216c
Angie Muñoz <aemunozpl@gmail.com>
2023-07-08 08:50:12 -0500

    first form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeCategoryComponent } from './category/home-category/home-category.component';
 import { ListCategoryComponent } from './category/list-category/list-category.component';
+import{ ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ListCategoryComponent } from './category/list-category/list-category.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/category/home-category/home-category.component.ts
+++ src/app/category/home-category/home-category.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { FormControl,FormGroup,FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-home-category',
   templateUrl: './home-category.component.html',
   styleUrls: ['./home-category.component.css']
 })
-export class HomeCategoryComponent {
+export class HomeCategoryComponent implements OnInit {
 
+  public formLogin!: FormGroup;
+
+  constructor(private fb:FormBuilder){
+    
+  }
+
+  ngOnInit():void{
+    this.formLogin=this.fb.group({
+      'email':['',[Validators.required,Validators.email]],
+      'password':['',[Validators.required,Validators.minLength(6)]],
+      'terms':['',[Validators.required],],
+    })
+  }
+
+  send():any{
+    console.log(this.formLogin.value)
+  }
 }
+
+  // favoriteColorControl = new FormControl('');
+
+
